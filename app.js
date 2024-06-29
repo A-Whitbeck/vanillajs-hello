852a6aa108122addc2305754992ec681d3b93cc3
Antonio Whitbeck <a.whitbeck89@gmail.com>
2024-06-29 03:33:08 +0000

    AntonioWhitbeck JS

---
+++ app.js
@@ -0,0 +1,22 @@
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < domain.length; c++) {
+          console.log(pronoun[i] + adj[a] + noun[b] + domain[c]);
+        }
+      }
+    }
+  }
+};
